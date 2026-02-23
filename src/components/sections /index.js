085cfc5c9c91a754d640cad43b8d42c98a5f8663
web3chima <166958944+web3chima@users.noreply.github.com>
2026-02-23 16:10:44 +0100

    Create index.js

---
+++ src/components/sections /index.js
@@ -0,0 +1,14 @@
+// Generic components
+export { Hero } from './Hero';
+export { Features } from './Features';
+export { CTA } from './CTA';
+
+// Cipher-specific components
+export { Header } from './Header';
+export { CipherHero } from './CipherHero';
+export { TrustBar } from './TrustBar';
+export { RiseOfRobots } from './RiseOfRobots';
+export { AccordionFeatures } from './AccordionFeatures';
+export { LogoStrip } from './LogoStrip';
+export { SDKShowcase } from './SDKShowcase';
+export { ContactCTA } from './ContactCTA';
