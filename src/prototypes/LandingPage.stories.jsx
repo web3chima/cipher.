de335ee001592cd6b416298958d872c23459dba7
web3chima <166958944+web3chima@users.noreply.github.com>
2026-02-24 02:56:08 +0100

    Create LandingPage.stories.jsx

---
+++ src/prototypes/LandingPage.stories.jsx
@@ -0,0 +1,11 @@
+import { LandingPage } from './LandingPage';
+
+export default {
+  title: 'Prototypes/Landing Page',
+  component: LandingPage,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Default = {};
