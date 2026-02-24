ea2fb48b23fbe694077124e8ef07a55152235583
web3chima <166958944+web3chima@users.noreply.github.com>
2026-02-24 02:51:30 +0100

    Create CipherLanding.stories.jsx

---
+++ src/prototypes/CipherLanding.stories.jsx
@@ -0,0 +1,11 @@
+import { CipherLanding } from './CipherLanding';
+
+export default {
+  title: 'Prototypes/Cipher Landing',
+  component: CipherLanding,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Default = {};
